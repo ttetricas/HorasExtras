e22f087e817e1eb322632521dcf204824705ae8b
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-05-09 22:30:06 -0300

    dificil

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("--Horas Extras--\n");
+
+Console.Write("Digite o salário por hora base: R$ ");
+double salarioHoraBase = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o número de horas trabalhadas no mês: ");
+double horasTrabalhadas = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o número de horas extras trabalhadas no mês: ");
+double horasExtras = Convert.ToDouble(Console.ReadLine());
+
+double salarioBase = salarioHoraBase * horasTrabalhadas;
+
+double valorHorasExtras = salarioHoraBase * 1.4;
+
+double valorHoraExtra = horasExtras * valorHorasExtras;
+
+double salarioTotal = salarioBase + valorHoraExtra;
+
+Console.WriteLine("O salário total a ser recebido é de: R$" + salarioTotal);
+
+
+
+
